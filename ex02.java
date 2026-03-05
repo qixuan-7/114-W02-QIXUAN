eae57c10c81797819883b41b88979a706f4d826d
qixuan <c112181138@nkust.edu.tw>
2026-03-05 11:42:54 +0800

    a

---
+++ ex02.java
@@ -0,0 +1,20 @@
+public class ex02{
+    public static void main(String[] args) {
+              int grade = 80;
+              char type = 'm';
+    if (grade >60 && type =='m'){
+         System.out.println( "Course:" + type + "\nYou" +grade);
+        }else{
+            System.out.println("課程成績不及格");
+        }
+    }
+    public static void main2(String[] args) {
+        
+    }
+    public static void main3(String[] args) {
+        
+    }
+    public static void main4(String[] args) {
+        
+    }
+}
